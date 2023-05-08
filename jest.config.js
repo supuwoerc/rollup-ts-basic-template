70d32d846f66f4b3dee88a988adccb2677e9ff47
zhangqm <qimeng_zhang@intsig.net>
2023-05-08 22:27:59 +0800

    chore: jest配置调整

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
-export default {
+module.exports = {
+    preset: 'ts-jest',
     clearMocks: true,
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
 }
