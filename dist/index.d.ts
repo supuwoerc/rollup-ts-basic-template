804611f616e05731ae2332bf4f17fe25f1f3ce7d
zhangqimeng <36561218+supuwoerc@users.noreply.github.com>
2024-01-17 02:27:02 +0000

    feat: tsc-alias&env-params

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare const TEMPLATE_INFO = "\u4F7F\u7528GithubActions&TypeDoc\u90E8\u7F72\u6587\u6863";
+/**
+ * @param a 数字1
+ * @param b 数字2
+ * @returns 数字1和数字2的求和结果
+ */
+export declare function addNumber(a: number, b: number): number;
--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,21 @@
-import dts from 'rollup-plugin-dts'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import alias from '@rollup/plugin-alias'
+import tscAlias from 'rollup-plugin-tsc-alias'
 import esbuild from 'rollup-plugin-esbuild'
 import typescript from 'rollup-plugin-typescript2'
 import babel from '@rollup/plugin-babel'
 import eslint from '@rollup/plugin-eslint'
 import { terser } from 'rollup-plugin-terser'
 import cleaner from 'rollup-plugin-cleaner'
+import replace from '@rollup/plugin-replace'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { config } from 'dotenv'
+
 const entries = ['src/index.ts']
+const envConfig = config({ path: `./.env.${process.env.NODE_ENV}` }).parsed
 
 const plugins = [
     eslint(),
@@ -32,11 +36,18 @@ const plugins = [
         ],
     }),
     json(),
-    typescript(),
+    typescript({
+        clean: true, // https://github.com/ezolenko/rollup-plugin-typescript2/issues/443
+    }),
     commonjs(),
     esbuild(),
+    replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify(envConfig.NODE_ENV),
+    }),
     terser(),
     cleaner({ targets: ['./dist/'], silent: false }),
+    tscAlias(),
 ]
 
 export default [
@@ -59,14 +70,5 @@ export default [
         ],
         external: [],
         plugins,
-    })),
-    ...entries.map((input) => ({
-        input,
-        output: {
-            file: input.replace('src/', '').replace('.ts', '.d.ts'),
-            format: 'esm',
-        },
-        external: [],
-        plugins: [dts({ respectExternal: true })],
     })),
 ]
